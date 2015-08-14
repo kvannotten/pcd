0d09e61ce4750e2a592506a82cd21a9360765b4b
Kristof Vannotten <me@kristof.io>
2015-08-14 19:50:28 +0200

    Refactored configuration to its own package

---
+++ configuration/configuration.go
@@ -0,0 +1,51 @@
+package configuration
+
+import (
+	"io/ioutil"
+	"log"
+	"os/user"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Commands Commands
+	Podcasts Podcasts
+}
+
+type Commands struct {
+	Player   string
+	Download string
+}
+
+type Podcast struct {
+	ID       int
+	Name     string
+	Feed     string
+	Username string
+	Password string
+}
+
+type Podcasts []Podcast
+
+func InitConfiguration() *Config {
+	var config Config
+
+	usr, err := user.Current()
+	if err != nil {
+		log.Println("Could not find user.")
+	}
+	homeDir := usr.HomeDir
+
+	source, err := ioutil.ReadFile(homeDir + "/.pcd")
+	if err != nil {
+		log.Println("Could not read configuration file.")
+	}
+
+	err = yaml.Unmarshal(source, &config)
+	if err != nil {
+		log.Println("Could not parse configuration file.")
+	}
+
+	return &config
+}
--- pcd.go
+++ pcd.go
@@ -2,56 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"os/user"
-
-	"gopkg.in/yaml.v2"
+	"github.com/kvannotten/pcd/configuration"
 )
 
 var (
-	Conf Config
+	Conf *configuration.Config
 )
 
 func main() {
-	InitConfiguration()
+	Conf = configuration.InitConfiguration()
 	fmt.Printf("%#v", Conf)
-}
-
-type Config struct {
-	Commands Commands
-	Podcasts Podcasts
-}
-
-type Commands struct {
-	Player   string
-	Download string
-}
-
-type Podcast struct {
-	ID       int
-	Name     string
-	Feed     string
-	Username string
-	Password string
-}
-
-type Podcasts []Podcast
-
-func InitConfiguration() {
-	usr, err := user.Current()
-	if err != nil {
-		log.Println("Could not find user.")
-	}
-	homeDir := usr.HomeDir
-
-	source, err := ioutil.ReadFile(homeDir + "/.pcd")
-	if err != nil {
-		log.Println("Could not read configuration file.")
-	}
-
-	err = yaml.Unmarshal(source, &Conf)
-	if err != nil {
-		log.Println("Could not parse configuration file.")
-	}
 }
